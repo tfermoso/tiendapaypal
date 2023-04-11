99c6e3fa1e055e4a66149dce7dbf9ee9435b657c
tfermoso <tomas.fermoso@gmail.com>
2023-04-11 17:30:03 +0200

    add mostrarCarrito.php

---
+++ mostrarCarrito.php
@@ -0,0 +1,21 @@
+<?php
+include("global/config.php");
+include("carrito.php");
+include("templates/cabecera.php");
+?>
+<h3>Lista del carrito</h3>
+<div class="table-responsive">
+    <table class="table table-primary">
+       
+        <tbody>
+            <tr class="">
+              Contenido del carrito...
+            </tr>
+           
+        </tbody>
+    </table>
+</div>
+
+<?php
+include("templates/pie.php")
+?>
---
+++ templates/pie.php
@@ -0,0 +1,4 @@
+</div>
+<div class="col-12 text-center">Formacom@ 2023</div>
+</body>
+</html>
