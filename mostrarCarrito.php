f6b68cc8a57f73cc502d2bc71d66a6123f1a442f
tfermoso <tomas.fermoso@gmail.com>
2023-04-11 19:22:35 +0200

    formateando importe

--- mostrarCarrito.php
+++ mostrarCarrito.php
@@ -23,8 +23,8 @@ include("templates/cabecera.php");
                     <tr>
                         <th scope="row"><?php echo $producto["Nombre"]; ?></th>
                         <td><?php echo $producto["Cantidad"]; ?></td>
-                        <td><?php echo $producto["Precio"]; ?></td>
-                        <td><?php echo number_format($producto["Cantidad"] * $producto["Precio"], 2); ?></td>
+                        <td><?php echo number_format($producto["Precio"],2,",","."); ?></td>
+                        <td><?php echo number_format($producto["Cantidad"] * $producto["Precio"], 2,",","."); ?></td>
                         <td>
                             <form action="" method="post">
                                 <input type="hidden" name="ID" id="id" value="<?php echo openssl_encrypt($producto["ID"], COD, KEY); ?>">
@@ -40,7 +40,7 @@ include("templates/cabecera.php");
                         <h3>Total</h3>
                     </td>
                     <td align="right">
-                        <h3><?php echo number_format($total, 2); ?></h3>
+                        <h3><?php echo number_format($total, 2,",","."); ?></h3>
                     </td>
                     <td></td>
 
