977b2cdc3b1f5f570a54a66958b6799d264accdc
tfermoso <tomas.fermoso@gmail.com>
2023-04-12 16:12:24 +0200

    añadiendo detalle venta

--- pagar.php
+++ pagar.php
@@ -16,6 +16,16 @@ if($_POST){
     $sentencia->bindParam(":correo",$_POST["email"]);
     $sentencia->bindParam(":total",$total);
     $sentencia->execute();
+    $idVenta=$pdo->lastInsertId();
+    foreach ($_SESSION["CARRITO"] as $indice=>$producto) {
+        $sentencia=$pdo->prepare("insert into tbldetalleventas values 
+        (NULL,".$idVenta.",:IDProducto,:Precio,:Cantidad,0)");
+        $sentencia->bindParam(":IDProducto",$producto["ID"]);
+        $sentencia->bindParam(":Cantidad",$producto["Cantidad"]);
+        $sentencia->bindParam(":Precio",$producto["Precio"]);
+        $sentencia->execute();
+    }
+
     echo "<h3>".$total."</h3>";
 }
 
