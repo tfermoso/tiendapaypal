13ec28d76ff19b73fbc5ee80472b4dbfae6c370e
tfermoso <tomas.fermoso@gmail.com>
2023-04-12 19:14:22 +0200

    login paypal

--- verificador.php
+++ verificador.php
@@ -1,11 +1,11 @@
 <?php 
-print_r($_GET);
-
+//print_r($_GET);
+include("global/config.php");
 $Login=curl_init("https://api-m.sandbox.paypal.com/v1/oauth2/token");
 curl_setopt($Login,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($Login,CURLOPT_USERPWD,CLIENTIDPAYPAL.":".SECRETPAYPAL);
 curl_setopt($Login,CURLOPT_POSTFIELDS,"grant_type=client_credentials");
 
 $result=json_decode(curl_exec($Login));
-print_r($result);
+print_r($result->access_token);
 ?>
