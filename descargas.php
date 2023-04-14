4774a148e93da8b9aae9f9f09f391124f61dadc3
tfermoso <tomas.fermoso@gmail.com>
2023-04-14 19:03:52 +0200

    descargas.php

---
+++ descargas.php
@@ -0,0 +1,3 @@
+<?php
+echo "Completado el pago y enviado el mail";
+?>
--- pagar.php
+++ pagar.php
@@ -115,6 +115,7 @@ if ($_POST) {
                         }).then(data => data.text())
                         .then(datos => {
                             console.log(datos);
+                            window.location="descargas.php";
                         }).catch(err => {
                             console.log(err);
                         })
