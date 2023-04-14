f536967efabf6dfbd7eb7de4a8063248612f9d18
tfermoso <tomas.fermoso@gmail.com>
2023-04-14 17:05:19 +0200

    actualizamos datos del pago

--- verificador2.php
+++ verificador2.php
@@ -1,8 +1,36 @@
 <?php
-$json=file_get_contents('php://input');
-$datos=json_decode($json);
+include("global/config.php");
+include("global/conexion.php");
+session_start();
+$json = file_get_contents('php://input');
+$data = json_decode($json);
 //Con los datos recibidos actualizar pago en la bbdd
-$custom=$datos->purchase_units[0]->reference_id;
-//print_r($datos);
-echo $json."-----";
-?>
+$custom = $data->datos->purchase_units[0]->reference_id;
+$custom_data = explode("#", $custom);
+$idVenta = openssl_decrypt($custom_data[1], COD, KEY);
+$status = $data->datos->status;
+$total = 0;
+$SID = session_id();
+foreach ($_SESSION["CARRITO"] as $indice => $producto) {
+    $total += $producto["Cantidad"] * $producto["Precio"];
+}
+$totalPaypal=$data->datos->purchase_units[0]->payments->captures[0]->amount->value;
+
+$respuesta=array();
+$respuesta["estado"]=$status;
+$estado="";
+if($status=="COMPLETED"){
+    if($total==$totalPaypal){
+        $respuesta["pago"]="completo";
+    }else{
+        $respuesta["pago"]="incompleto";
+    }
+}
+$consulta = "UPDATE tblventas SET PaypalDatos = :paypaldatos,Status = :estado WHERE tblventas.ID = :IDVenta;";
+$sentencia=$pdo->prepare($consulta);
+$sentencia->bindParam(":paypaldatos",$json);
+$sentencia->bindParam(":estado",$status);
+$sentencia->bindParam(":IDVenta",$idVenta);
+$sentencia->execute();
+
+echo json_encode($respuesta);
